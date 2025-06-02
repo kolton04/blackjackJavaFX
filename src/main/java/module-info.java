023a80fd7cef35f29d1558500c8cc5f39df8101c
Kolton Tolliver <kolton.tolliver@gmail.com>
2025-06-01 21:41:13 -0400

    more pom.xml fixes

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.koltont.blackjack {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires com.jfoenix;
-
-
-    opens com.koltont.blackjack to javafx.fxml;
-    exports com.koltont.blackjack;
-}
